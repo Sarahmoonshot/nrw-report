928135bbfbc8e4caec9f5f0f83bd7f97e8f02de7
marc <marc.moonshot@gmail.com>
2025-08-13 06:12:25 +0000

    added sveltekit frontend

---
+++ nrw-app/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  let count = $state(0)
+
+  const increment = () => {
+    count++
+  }
+</script>
+
+<button
+  onclick={increment}
+  class={"bg-amber-200 p-1 px-3 hover:bg-amber-300 rounded-md"}
+>
+  {count}
+</button>
